2bf02b0ddb9ef8556b5dc13e4eb0fd9714cb79cc
Thom9521 <Thom9521@edu.easj.dk>
2018-05-25 11:38:08 +0200

    Kodning i MainActivity

--- app/src/main/java/biolaer/dk/biolaer/MainActivity.java
+++ app/src/main/java/biolaer/dk/biolaer/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //He1
+
+        Button confirmBtn = (Button) findViewById(R.id.confirmBtn);
+        Spinner categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
 
     }
 }
