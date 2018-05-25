a4ac4c9b0a409ffb1d2d5ce58378eb3af3bf454c
Michael Trans <mich282q@edu.easj.dk>
2018-05-25 10:09:10 +0200

    Oprettet Activity

---
+++ app/src/main/java/biolaer/dk/biolaer/LevelActivity.java
@@ -0,0 +1,13 @@
+package biolaer.dk.biolaer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+}
