5ba8932b5e8d5aeb933af6ddd050320f5f294537
Michael Trans <mich282q@edu.easj.dk>
2018-05-25 11:20:52 +0200

    Test

--- app/src/main/java/biolaer/dk/biolaer/MainActivity.java
+++ app/src/main/java/biolaer/dk/biolaer/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hej
+        //He1
 
     }
 }
