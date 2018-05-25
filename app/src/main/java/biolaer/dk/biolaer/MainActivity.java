09df91852f149e60893f438616873537a0f1025b
Thom9521 <Thom9521@edu.easj.dk>
2018-05-25 10:43:12 +0200

    Fjernet ting fra seb

--- app/src/main/java/biolaer/dk/biolaer/MainActivity.java
+++ app/src/main/java/biolaer/dk/biolaer/MainActivity.java
@@ -2,6 +2,7 @@ package biolaer.dk.biolaer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
 
     }
 }
