44f18708aa5c36f283f6022ce3c5ebdf5697923c
Thom9521 <Thom9521@edu.easj.dk>
2018-05-29 15:56:47 +0200

    Nu crasher appen ikke i EasyHsActivity :D

--- app/src/main/java/biolaer/dk/biolaer/EasyHsActivity.java
+++ app/src/main/java/biolaer/dk/biolaer/EasyHsActivity.java
@@ -1,6 +1,8 @@
 package biolaer.dk.biolaer;
 
 import android.content.pm.ActivityInfo;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -34,12 +37,34 @@ public class EasyHsActivity extends AppCompatActivity {
 
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference DBnavn = mDatabase.child("highscore");
+        DatabaseReference DBnavn_2 = DBnavn.child("highscore_easy");
+        DatabaseReference DBnavn_3 = DBnavn_2.child("id1");
+        DatabaseReference DBnavn_4 = DBnavn_3.child("navn");
+
         scoreList_dynamic = (ListView) findViewById(R.id.scoreList_dynamic);
 
 
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listNavn);
         scoreList_dynamic.setAdapter(arrayAdapter);
 
+        DBnavn_4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String value = dataSnapshot.getValue(String.class);
+                listNavn.add(value);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        /*
         mDatabase.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -79,6 +104,7 @@ public class EasyHsActivity extends AppCompatActivity {
                 EasyHsActivity.super.onBackPressed();
             }
         });
+        */
 
 
 
