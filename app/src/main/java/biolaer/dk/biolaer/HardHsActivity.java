7dd38722b5ded69c4112e852ca188fe371d68beb
dani832m <31951102+dani832m@users.noreply.github.com>
2018-05-29 12:39:51 +0200

    XML-filerne for Easy og Hard er optimeret til Cconstrait Layout Button sat ind i EashHsActivity og HardHsActivity

--- app/src/main/java/biolaer/dk/biolaer/HardHsActivity.java
+++ app/src/main/java/biolaer/dk/biolaer/HardHsActivity.java
@@ -3,6 +3,8 @@ package biolaer.dk.biolaer;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HardHsActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class HardHsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hard_hs);
         // Tvinger activityen til at være i Portrait orientation mode.
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Button returnBtn = (Button) findViewById(R.id.returnBtn);
+
+        //Metode som får returnBtn til at hoppe tilbage til aktiviteten, som var før den nuværende.
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HardHsActivity.super.onBackPressed();
+            }
+        });
     }
 }
